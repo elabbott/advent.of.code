8708e45d8289cf9dafa72abbf6d0348d1a77c3e9
Eric Abbott <ericabbott@duck.com>
2023-11-26 15:37:10 -0500

    day 3 part 1 complete

--- 2022/golang/day_03/main.go
+++ 2022/golang/day_03/main.go
@@ -1,9 +1,16 @@
 package main
 
-// import (
-// 	day_03 "day_03/helper"
-// )
+import (
+	day_03 "day_03/helper"
+)
 
 func main() {
 
+	var inputData = day_03.GetInputFromFile("./input.example.txt")
+
+	if day_03.DayThree_A(inputData) != 157 {
+		panic("DayThree_A() failed, expected 157")
+	}
+	inputData = day_03.GetInputFromFile("./input.txt")
+	println(day_03.DayThree_A(inputData))
 }
