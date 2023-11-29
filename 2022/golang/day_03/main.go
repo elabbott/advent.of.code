0b7ea43591de5f5e633bbf3b9496222ece206e59
Eric Abbott <ericabbott@duck.com>
2023-11-28 19:07:38 -0500

    day 3 part 2 add to main

--- 2022/golang/day_03/main.go
+++ 2022/golang/day_03/main.go
@@ -6,13 +6,30 @@ import (
 
 func main() {
 
+	doA()
+
+	doB()
+
+}
+
+func doA() {
+
 	var inputData = day_03.GetInputFromFile("./input.example.txt")
 
 	if day_03.DayThree_A(inputData) != 157 {
 		panic("DayThree_A() failed, expected 157")
 	}
-
 	inputData = day_03.GetInputFromFile("./input.txt")
 	println(day_03.DayThree_A(inputData))
+}
+
+func doB() {
+
+	var inputData = day_03.GetInputFromFile("./input.example.txt")
+	if day_03.DayThree_B(inputData) != 70 {
+		panic("DayThree_B() failed, expected 70")
+	}
+	inputData = day_03.GetInputFromFile("./input.txt")
+	println(day_03.DayThree_B(inputData))
 
 }
